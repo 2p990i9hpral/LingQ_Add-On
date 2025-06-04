5b96e7c334ff3d8f26dab6f5f170da7022c44ecd
2p990i9hpral <4fkq3kpnszzo@protonmail.com>
2025-06-04 17:54:03 +0900

    Change the style of the send chat button.

--- lingq_add-on.js
+++ lingq_add-on.js
@@ -5,7 +5,7 @@
 // @match        https://www.lingq.com/*/learn/*/web/library/course/*
 // @match        https://www.youtube-nocookie.com/*
 // @match        https://www.youtube.com/embed/*
-// @version      5.12.2
+// @version      5.12.3
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @namespace https://greasyfork.org/users/1458847
@@ -1175,22 +1175,25 @@
         
                 .input-container {
                     display: flex;
-                    margin-bottom:10px;
+                    margin-bottom: 10px;
+                    border: 1px solid rgb(125 125 125 / 35%);
+                    border-radius: 5px;
                 }
         
                 #user-input {
                     flex-grow: 1;
                     padding: 5px 10px;
-                    margin-right: 5px;
-                    border: 1px solid rgb(125 125 125 / 35%);
-                    border-radius: 5px;
                     font-size: 0.9rem;
+                    background: none !important;
+                    outline: none;
+                }
+                
+                #user-input::placeholder {
+                    color: rgb(125 125 125) !important;
                 }
         
                 #send-button {
                     padding: 5px 10px;
-                    border: 1px solid rgb(125 125 125 / 35%);
-                    border-radius: 5px;
                 }
         
                 .chat-message {
@@ -2818,7 +2821,7 @@
                 const chatContainer = createElement("div", { id: "chat-container" });
                 const inputContainer = createElement("div", { className: "input-container" });
                 const userInput = createElement("input", { type: "text", id: "user-input", placeholder: "Ask anything" });
-                const sendButton = createElement("button", { id: "send-button", textContent: "Send" });
+                const sendButton = createElement("button", { id: "send-button" , innerHTML: `<svg width="17" height="17" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="#6B6C7B" d="M481.508,210.336L68.414,38.926c-17.403-7.222-37.064-4.045-51.309,8.287C2.86,59.547-3.098,78.551,1.558,96.808 L38.327,241h180.026c8.284,0,15.001,6.716,15.001,15.001c0,8.284-6.716,15.001-15.001,15.001H38.327L1.558,415.193 c-4.656,18.258,1.301,37.262,15.547,49.595c14.274,12.357,33.937,15.495,51.31,8.287l413.094-171.409 C500.317,293.862,512,276.364,512,256.001C512,235.638,500.317,218.139,481.508,210.336z"></path></svg>`});
 
                 inputContainer.appendChild(userInput);
                 inputContainer.appendChild(sendButton);
