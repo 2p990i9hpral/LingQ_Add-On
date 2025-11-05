22724951db08087559c19a22d0178ec836b2fd85
DK <4fkq3kpnszzo@protonmail.com>
2025-11-06 01:30:25 +0900

    Fixed an issue related to video size.

--- lingq_add-on.js
+++ lingq_add-on.js
@@ -4,7 +4,7 @@
 // @match        https://www.lingq.com/*
 // @match        https://www.youtube-nocookie.com/*
 // @match        https://www.youtube.com/embed/*
-// @version      8.1.4
+// @version      8.1.5
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @namespace https://greasyfork.org/users/1458847
